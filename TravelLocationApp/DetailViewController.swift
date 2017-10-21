f9e81c822ce14168c06d8ee8738bc08fa8ce20a3
Anthony Nicholas <s3607162@student.rmit.edu.au>
2017-10-20 18:14:37 -0700

    implemented map view & shows lat and long of selected place

--- TravelLocationApp/DetailViewController.swift
+++ TravelLocationApp/DetailViewController.swift
@@ -19,8 +19,10 @@ class DetailViewController: UIViewController, UIImagePickerControllerDelegate, U
     @IBOutlet weak var placeDescriptionLabel: UITextField!
     
     @IBOutlet weak var map: MKMapView!
-    
     @IBOutlet weak var mapType: UISegmentedControl!
+    @IBOutlet weak var latTextField: UITextField!
+    @IBOutlet weak var longTextField: UITextField!
+    
     
     weak var place: Place?
     
@@ -32,6 +34,7 @@ class DetailViewController: UIViewController, UIImagePickerControllerDelegate, U
         placeDescriptionLabel.text = place?.desc
         PlaceImage.image = UIImage(data: place?.image! as! Data)
         placeDescriptionLabel.delegate = self
+        showMap(sender: mapType)
     }
     
     //Handle image selection by user
@@ -77,6 +80,58 @@ class DetailViewController: UIViewController, UIImagePickerControllerDelegate, U
         return true
     }
 
+    @IBAction func showMap(_ sender: Any) {
+        
+        switch(mapType.selectedSegmentIndex)
+        {
+        case 0:
+            map.mapType = MKMapType.standard
+        case 1:
+            map.mapType = MKMapType.satellite
+        case 2:
+            map.mapType = MKMapType.hybrid
+        default:
+            map.mapType = MKMapType.standard
+        }
+        
+        let geoCoder = CLGeocoder();
+        let addressString = place?.name
+        CLGeocoder().geocodeAddressString(addressString!, completionHandler:
+            {(placemarks, error) in
+                
+                if error != nil {
+                    print("Geocode failed: \(error!.localizedDescription)")
+                } else if placemarks!.count > 0 {
+                    let placemark = placemarks![0]
+                    let location = placemark.location
+                    let coords = location!.coordinate
+                    self.latTextField.text = String(coords.latitude)
+                    self.longTextField.text = String(coords.longitude)
+                    
+                    let span = MKCoordinateSpanMake(0.05, 0.05)
+                    let region = MKCoordinateRegion(center: placemark.location!.coordinate, span: span)
+                    self.map.setRegion(region, animated: true)
+                    let ani = MKPointAnnotation()
+                    ani.coordinate = placemark.location!.coordinate
+                    ani.title = placemark.locality
+                    ani.subtitle = placemark.subLocality
+                    self.map.addAnnotation(ani)
+                }
+        })
+        
+        // add an annotation
+//        let annotation = MKPointAnnotation()
+//        annotation.coordinate = coordinates
+//        annotation.title = "Phoenix"
+//        annotation.subtitle = "AZ"
+//
+//        self.map.addAnnotation(annotation)
+        
+    }
+    
+    
+    
+    
     
     
     override func didReceiveMemoryWarning() {
